d3f82a88577e16e2b28916fc4e623cb497c035e4
SoukainaSabbaji <sabbajisoukaina@gmail.com>
2022-12-29 16:00:31 +0100

    started working on cpp06

---
+++ mod06/ex00/convert.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   convert.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssabbaji <ssabbaji@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 13:30:41 by ssabbaji          #+#    #+#             */
+/*   Updated: 2022/12/29 13:41:41 by ssabbaji         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONVERT_HPP
+#define CONVERT_HPP
+
+#include <iostream>
+#include <string>
+#include <limits>
+#include <cstring>
+#include <cmath>
+
+
+#define RED   "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW   "\x1b[33m"
+#define BLUE   "\x1b[34m"
+#define MAGENTA   "\x1b[35m"
+#define CYAN   "\x1b[36m"
+#define RESET "\x1b[0m"
+
+#endif
