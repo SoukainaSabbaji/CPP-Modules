f47fabc4b44d1304450fd8af3661cf7a8397c648
SoukainaSabbaji <sabbajisoukaina@gmail.com>
2023-01-01 17:37:15 +0100

    rje3 liyya module :CCCCCC

--- mod06/ex00/main.cpp
+++ mod06/ex00/main.cpp
@@ -6,46 +6,22 @@
 /*   By: ssabbaji <ssabbaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 13:20:34 by ssabbaji          #+#    #+#             */
-/*   Updated: 2022/12/30 14:26:20 by ssabbaji         ###   ########.fr       */
+/*   Updated: 2023/01/01 16:03:13 by ssabbaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Converter.hpp"
 
-// int main(int argc, char **argv)
-// {
-//     Converter c;
-//     if (argc != 2)
-//     {
-//         std::cout << RED <<  "wrong ! usage : ./convert <literal> " << RESET << std::endl;
-//         return (1);
-//     }
-//     std::string literal = argv[1];
-//     //test the conversion to char
-//     try
-//     {
-//         c = Converter(literal);
-//         std::cout << "char: ";
-//         std::cout << static_cast<char>(c) << std::endl;
-//     }
-//     catch (std::exception &e)
-//     {
-//         std::cout << "char: " << e.what() << std::endl;
-//     }
-// }
-
 int main(int argc, char **argv)
 {
-    	if (argc != 2)
+    if (argc != 2)
 	{
-		std::cerr << "Wrong number of arguments\n";
+		std::cerr << RED << "Wrong number of arguments" << RESET << std::endl;
 		return (0);
 	}
 	std::cout << std::fixed << std::setprecision(1); // Precision = 1
 	std::string	argument = argv[1];
-	
 	Converter	converter(argument);
-
 	char		c;
 	int			n;
 	float		f;
@@ -55,4 +31,5 @@ int main(int argc, char **argv)
 	n = static_cast<int>(converter);
 	f = static_cast<float>(converter);
 	d = static_cast<double>(converter);
+
 }
